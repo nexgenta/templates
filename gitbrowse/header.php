c5765490352d86de0841f5018e3e1ac6808d0636
nevali <mo@nevali.net>
2011-01-23 18:18:52 +0000

    Various gitbrowse clean-ups.

--- gitbrowse/header.php
+++ gitbrowse/header.php
@@ -8,5 +8,9 @@ $global_nav = array(
 	'baird' => array('link' => 'http://projectbaird.com/', 'name' => 'Project Baird'),
 	);
 
+function format_time($dt)
+{
+	return strftime('%B %d, %Y', $dt);
+}
 
 require_once(dirname(__FILE__) . '/../nexgenta/header.php');
